b1007a710ae32f25055f4d9dcbe139d0a1b962f8
Karan-666 <karankumar00619@gmail.com>
2025-10-21 18:48:43 +0530

    feat: added footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer() {
+  return (
+    // Placeholder for the application footer.
+    <div className="Footer-Container">
+      <h1>Footer Component</h1>
+    </div>
+  )
+}
+
+export default Footer;
