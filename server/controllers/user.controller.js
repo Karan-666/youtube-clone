0e73904df4b1456102cd8422b5e519c4df7a4296
Karan-666 <karankumar00619@gmail.com>
2025-10-21 10:56:19 +0530

    Added logic of register user with bcrypt hasing

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,12 +1,50 @@
 
+import mongoose from "mongoose";
 import UserModel from "../models/User.model.js";
+
+//Import bcrypt for hashing (encrypting) user passwords.
+import bcrypt from 'bcrypt';
 
 export async function register(req, res){
 
-    return res.status(200).json({
-        message: "Registration controller reached successfully!",
-        data: req.body
-    })
+    try{
+
+        //Destructure the data sent from the frontend in the request body.
+        const {username, email, password} = req.body;
+
+        //Find one user where the email matches the provided email.
+        const emailCheck = await UserModel.findOne({email});
+
+        // checking if user already exist
+        if(emailCheck){
+            return res.status(409).json({message: "Email already exist"});
+        }
+
+        //password hashing
+        // takes 2 argument, password and salt rounds (taken as 10)
+        const hashedPassword = bcrypt.hashSync(password, 10)
+
+        // creating new user with hashed password
+        const newUser = await UserModel.create({
+            username, // if key and value is same, no need write twice
+            email,
+            password : hashedPassword // Store the secure hash, NOT the plain text password.
+        })
+
+        // sending success reponse
+         return res.status(201).json({
+        message: "User registered successfully!",
+            user: { 
+                id: newUser._id,
+                username: newUser.username,
+                email: newUser.email,
+            }
+        });
+    } catch (err) {
+        // Handle any server or database errors.
+        console.error("Registration error:", err);
+        return res.status(500).json({ message: "Internal server error during registration." });
+    }
 }
 
 export async function login(req, res) {
