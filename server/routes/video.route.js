cc94086a1223ef6f8278cd646fed062279699d45
Karan-666 <karankumar00619@gmail.com>
2025-10-21 12:40:23 +0530

    test: test crud operation of video

--- server/routes/video.route.js
+++ server/routes/video.route.js
@@ -2,6 +2,7 @@
 
 import { createVideo } from "../controllers/video.controller.js";
 import { fetchAllVideos } from "../controllers/video.controller.js";
+import { updateVideo, deleteVideo } from "../controllers/video.controller.js";
 import verifyToken from "../middleware/verifyToken.js";
 
 // This function takes the Express app object and attaches the routes to it.
@@ -14,6 +15,13 @@ function videoRoutes(app) {
   //Route to create a new video
   // The request must pass through verifyToken before it hits the createVideo function.
   app.post("/api/video", verifyToken, createVideo);
+
+  // Route to update an existing video (Protected Update/PATCH operation).
+    // The ':id' is a dynamic parameter that captures the video ID from the URL.
+    app.patch('/api/video/:id', verifyToken, updateVideo);
+
+    // Route to delete a video.
+    app.delete('/api/video/:id', verifyToken, deleteVideo);
 }
 
 // Export the function so it can be imported and executed in our index.js file.
